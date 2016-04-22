92c80e93ef143cb98010f1a8bd16e82e0b06cf85
sensei100 <jenanton01@gmail.com>
2016-04-22 18:53:30 +0300

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+def largest_prime_factor(n)
+	p = 2
+	largest = 0
+
+while p <= n
+	if n % p == 0
+		while (n % p == 0)
+			n = n / p
+			largest = p
+			p +=1
+end
+end
+
+p += 1
+end
+
+return largest
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,31 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+def initialize(num)
+    @num = num
+    @p = 2
+    @factor_product = 1 
+end
+
+def number(number = @num)
+    until @factor_product == @num 
+        if number % @p == 0 
+                @factor_product *= @p 
+                number = number / @p 
+        else
+                @p += 1 
+        end
+    end
+    @p 
+end
+
+def to_s
+    @num
+end
+end
+
+
+
+
+
+
